a09089f2bdd52e34251fc15f87ba691c71c03746
Venkata-Sanjeeva <kuntumallavenkatasanjeeva2005@gmail.com>
2026-04-02 09:44:28 +0530

    Initial Commit

---
+++ src/main/java/com/example/timeManagementApi/TimeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagementApiApplication.class, args);
+	}
+
+}
