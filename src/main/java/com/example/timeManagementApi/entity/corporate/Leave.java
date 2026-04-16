8b0da3dc8a28598e181d2733c531a701a313be2a
Venkata-Sanjeeva <kuntumallavenkatasanjeeva2005@gmail.com>
2026-04-16 19:18:28 +0530

    fix(leave_date): Add logic to avoid duplicate leave dates tot 1 employee

--- src/main/java/com/example/timeManagementApi/entity/corporate/Leave.java
+++ src/main/java/com/example/timeManagementApi/entity/corporate/Leave.java
@@ -7,12 +7,19 @@ import com.example.timeManagementApi.util.IdentifierGenerator;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
-@Table(name = "leaves")
+@Table(
+		name = "leaves",
+		uniqueConstraints = {
+		        @UniqueConstraint(columnNames = {"leaveDate", "employee_id"})
+		    }
+)
 @Entity
 @Getter
 @Setter
@@ -25,5 +32,6 @@ public class Leave {
 	private LocalDate leaveDate;
 	
 	@ManyToOne
+	@JoinColumn(name = "employee_id") // Explicitly naming the column for the constraint
 	private Employee employee;
 }
--- src/main/java/com/example/timeManagementApi/repository/corporate/LeaveRepository.java
+++ src/main/java/com/example/timeManagementApi/repository/corporate/LeaveRepository.java
@@ -1,5 +1,6 @@
 package com.example.timeManagementApi.repository.corporate;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import com.example.timeManagementApi.entity.corporate.Leave;
 public interface LeaveRepository extends JpaRepository<Leave, String> {
 	List<Leave> findByEmployeeId(String empId);
 	List<Leave> findByEmployeeIdOrderByLeaveDateDesc(String empId);
+	boolean existsByEmployeeEmailAndLeaveDate(String empEmail, LocalDate leaveDate);
 }
